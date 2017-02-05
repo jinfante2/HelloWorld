6d664e94dc73cb28a1343ea94531e5d0f56bc653
aktunc-STMU <aktunc@gmail.com>
2017-02-05 00:26:47 -0600

    Incorrect result. Check comments

--- Drink.java
+++ Drink.java
@@ -9,6 +9,8 @@ public class Drink
 		int customers = 12467;
 		double perWeek = (.14*customers);
 		double citrusDrink = (.64*customers);
+		/*this is not correct as 64 percent of energy drink users prefer citrus not 
+		64 percent of the customers, should be .14 * .64 * customers. Also include spaces on both sides of operators*/ 
 		
 		System.out.println("The approximate number of customers who purchase one or more drinks per week is " +(int)perWeek);
 		System.out.println("The approximate number of customers who prefer citrus flavored energy drinks is "+(int)citrusDrink);
